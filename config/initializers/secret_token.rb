60beeb1a3f2d9b2a7e59fc4094ba490e9682ae5f
Fraser Deans <fraser@shutl.co.uk>
2012-03-19 16:05:53 +0000

    sample app for using kss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blah::Application.config.secret_token = '434479367bc7990f46d06221b5e2bf74cd9d30fa0be42e41fd3a9758dfe817cec1dcbecee3e33dc509fffa5c3de244237ae7cc6ecc53aedee1b6fd11c828af2e'
